7dd7f76f2ed7194865fdda36440f44afca1889b0
Bartek Bak <bartek.bak@protonmail.com>
2025-04-16 16:54:58 +0100

    feat: add Tailwind CSS

---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <button className="font-plex-mono font-medium cursor-pointer border border-gray-200 rounded-lg px-3.5 py-2 active:bg-gray-100 hover:bg-gray-50">
+      PRESS ME
+    </button>
+  );
+}
+
+export default Button;
